939c7e076037386e231b6803a2aa327443b6bf98
keithma <keithma@bu.edu>
2015-04-15 15:08:46 -0400

    Stripped ice model down to the bare minimum.
    I realized that I should start with a simple, functional model that can run a
    benchmark test sucessfully. Once I have this, I can add in funcionality one
    component at a time. With this in mind, I have stripped out all but the
    essential components of the ice model.

--- src/ice.f90
+++ src/ice.f90
@@ -22,54 +22,45 @@ public ice_type
     logical :: on ! enable/disable model
     logical :: write_h ! output flag, ice thickness
     logical :: write_vd ! output flag, ice defm veolcity
-    logical :: write_vs ! output flag, ice sliding veolcity
-    logical :: write_ero ! output flat, erosion rate
     integer :: nx ! num grid pts in x-dir, [1]
     integer :: ny ! num grid pts in y-dir, [1]
     real(dp) :: c_b ! ice defm coeff, [Pa-3 a-1]
-    real(dp) :: c_bs ! ice sliding coeff, [Pa-3 a-1 m-2]
-    real(dp) :: c_nstress_base ! basal normal stress coeff, [1]
-    real(dp) :: c_constr ! constriction factor coeff, [m]
-    real(dp) :: c_heat_q_base ! basal heat flux, [W m-2]
-    real(dp) :: c_heat_k_ice ! ice conductivity, [W m-1 K-1]
-    real(dp) :: c_ero ! glacial erosion coeff [?]
     character(len=100) :: nbcName ! north BC name
     character(len=100) :: sbcName ! south BC name
     character(len=100) :: wbcName ! west BC name
     character(len=100) :: ebcName ! east BC name
-    character(len=100) :: solnName ! exact soln name
-    procedure(bc), pointer, nopass :: nbc ! set north BC
-    procedure(bc), pointer, nopass :: sbc ! set south BC
-    procedure(bc), pointer, nopass :: wbc ! set west BC
-    procedure(bc), pointer, nopass :: ebc ! set east BC
-    procedure(soln), pointer, pass :: solve ! compute soln
-!  contains
-!    procedure, pass :: init ! initialize all components
-!    procedure, pass :: run ! run model                   
+    character(len=100) :: solverName ! ice flow method name
+    procedure(bc_tmpl), pointer, nopass :: nbc ! set north BC
+    procedure(bc_tmpl), pointer, nopass :: sbc ! set south BC
+    procedure(bc_tmpl), pointer, nopass :: wbc ! set west BC
+    procedure(bc_tmpl), pointer, nopass :: ebc ! set east BC
+    procedure(flow_tmpl), pointer, pass :: flow ! ice flow  
+  contains
+    procedure, pass :: init ! initialize all components
+    procedure, pass :: run ! run model                   
   end type ice_type
 
 
   ! ---------------------------------------------------------------------------
-  ! SUB TEMPLATE: common form for the bc functions
+  ! FUNC TEMPLATE: common form for the bc functions
   ! ---------------------------------------------------------------------------
-  abstract interface
-    subroutine bc()
+  abstract interface 
+    function bc_tmpl()
       import :: dp ! use special types
-    end subroutine bc 
+    end function bc_tmpl 
   end interface
 
   ! ---------------------------------------------------------------------------
-  ! SUB TEMPLATE: common form for exact solutions
+  ! SUB TEMPLATE: common form for the bc functions
   ! ---------------------------------------------------------------------------
-  abstract interface
-    subroutine soln(i)
+  abstract interface 
+  subroutine flow_tmpl(i)
       import :: dp, ice_type ! use special types
-      class(ice_type), intent(inout) :: i
-    end subroutine soln
+    class(ice_type), intent(inout) :: i
+    end subroutine flow_tmpl 
   end interface
 
 contains
-  
 
   ! --------------------------------------------------------------------------- 
   ! SUB: initialize an ice model object
@@ -86,6 +77,5 @@ contains
   subroutine run(i)
     class(ice_type) :: i
   end subroutine run
-
 
 end module ice_module
