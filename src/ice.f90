0768fb853af80a958bbf865a794dd4278302a015
keithma <keithma@bu.edu>
2015-05-06 23:21:56 -0400

    Skeleton of ice model object is completed.

--- src/ice.f90
+++ src/ice.f90
@@ -26,64 +26,50 @@ public ice_type
   type ice_type
     logical :: on ! enable/disable ice flow
     logical :: on_soln ! enable/disable solution calculation
-    ! procedure for flow
-    ! procedure for solution
-
-
-    !logical :: on ! enable/disable
-    !logical :: on_soln ! enable disable exact solution
-    !real(rp) :: A0 ! ice flow law coeff prefactor, [Pa-3 a-1]
-    !real(rp) :: As0 ! ice sliding law coeff prefactor, [?]
-    !real(rp), allocatable :: A(:,:) ! ice flow law coeff, [Pa-3 a-1]
-    !real(rp), allocatable :: As(:,:) ! ice sliding law coeff, [?]
-    !character(len=100) :: name_flow ! ice flow method name
-    !character(len=100) :: name_soln ! exact solution name
-    !real(rp), dimension(10) :: param_soln !  solution param, [various]
-    !procedure(bc_tmpl), pointer, nopass :: apply_nbc ! set north BC
-    !procedure(bc_tmpl), pointer, nopass :: apply_sbc ! set south BC
-    !procedure(bc_tmpl), pointer, nopass :: apply_wbc ! set west BC
-    !procedure(bc_tmpl), pointer, nopass :: apply_ebc ! set east BC
-    !procedure(flow_tmpl), pointer, pass :: flow  ! ice flow  
-    !procedure(soln_tmpl), pointer, pass :: solve ! exact solution  
-!  contains
-!    procedure, pass :: init ! initialize all components
-!    procedure, pass :: update ! run model                   
+    procedure(bc_tmpl), pointer, nopass :: apply_nbc ! set north BC
+    procedure(bc_tmpl), pointer, nopass :: apply_sbc ! set south BC
+    procedure(bc_tmpl), pointer, nopass :: apply_wbc ! set west BC
+    procedure(bc_tmpl), pointer, nopass :: apply_ebc ! set east BC
+    procedure(update_tmpl), pointer, nopass :: update ! ice model method
+    procedure(soln_tmpl), pointer, nopass :: solve ! exact solution  
+  contains
+    procedure, pass :: init ! initialize object
   end type ice_type
 
 
   ! ---------------------------------------------------------------------------
-  ! FUNC TEMPLATE: common form for the bc functions
+  ! TEMPLATE: common form for the bc functions
   ! ---------------------------------------------------------------------------
   abstract interface 
-    function bc_tmpl(topo_edge, topo_intr, topo_oppo, ice_edge, ice_intr, &
-                     ice_oppo) result(bnd)
+    function bc_tmpl(topo_edge, topo_intr, topo_oppo, &
+                     ice_edge, ice_intr, ice_oppo) result(bnd)
       import :: rp                                       ! use special types
       real(rp), intent(in) :: topo_edge(:), ice_edge(:)  ! domain edge 
       real(rp), intent(in) :: topo_intr(:), ice_intr(:)  ! domain edge-1
       real(rp), intent(in) :: topo_oppo(:), ice_oppo(:)  ! opposite domain edge
-      real(rp) :: bnd(size(edge))                        ! bc points
+      real(rp) :: bnd(size(topo_edge))                   ! bc points
     end function bc_tmpl 
   end interface
 
 
   ! ---------------------------------------------------------------------------
-  ! SUB TEMPLATE: common form for the ice flow subroutines
+  ! TEMPLATE: common form for the ice model method
   ! ---------------------------------------------------------------------------
   abstract interface 
-    subroutine flow_tmpl(g, s)
-      import :: ice_type, state_type       ! use special types
-      class(ice_type), intent(in) :: g     ! ice model spec
+    subroutine update_tmpl(p, s)
+      import :: param_type, state_type     ! use special types
+      type(param_type), intent(in)    :: p ! parameters
       type(state_type), intent(inout) :: s ! state vars
-    end subroutine flow_tmpl 
+    end subroutine update_tmpl 
   end interface
 
   ! ---------------------------------------------------------------------------
-  ! SUB TEMPLATE: common form for the exact solution subroutines
+  ! TEMPLATE: common form for the exact solution subroutines
   ! ---------------------------------------------------------------------------
   abstract interface 
-    subroutine soln_tmpl(g, s)
-      import :: ice_type, state_type       ! use special types
-      class(ice_type), intent(in) :: g     ! ice model spec
+    subroutine soln_tmpl(p, s)
+      import :: param_type, state_type     ! use special types
+      type(param_type), intent(in)    :: p ! parameters
       type(state_type), intent(inout) :: s ! state vars
     end subroutine soln_tmpl 
   end interface
@@ -96,71 +82,85 @@ public ice_type
 contains
 
 
-  !! ---------------------------------------------------------------------------
-  !! SUB: Initialize ice model object 
-  !! ---------------------------------------------------------------------------
-  !subroutine init(g, s)
+  ! ---------------------------------------------------------------------------
+  ! SUB: Initialize ice model object 
+  ! ---------------------------------------------------------------------------
+  subroutine init(g, p)
 
-  !  class(ice_type), intent(inout) :: g
-  !  type(state_type), intent(in) :: s
+    class(ice_type), intent(out) :: g
+    type(param_type), intent(in) :: p
 
-  !  ! Allocate arrays
-  !  allocate(g%A(s%nx+2, s%ny+2))
-  !  allocate(g%As(s%nx+2, s%ny+2))
+    character(len=100) :: nbc_name, ebc_name, sbc_name, wbc_name
+    integer :: comma(3)
 
-  !  ! Populate arrays
-  !  g%A = g%A0
-  !  g%As = g%As0
+    ! Set ice model method
+    select case (p%ice_name)
 
-  !  ! Set boundary condition procedures (uses subroutine due to repitition)
-  !  call set_bc_pointer(g%name_nbc, g%apply_nbc)
-  !  call set_bc_pointer(g%name_sbc, g%apply_sbc)
-  !  call set_bc_pointer(g%name_ebc, g%apply_ebc)
-  !  call set_bc_pointer(g%name_wbc, g%apply_nbc)
-  !  
-  !  ! Set ice flow procedure
-  !  select case (g%name_flow)
-  !    case ('mahaffy')
-  !      g%on_soln = .true.
-  !      g%flow => NULL() ! NOT YET IMPLEMENTED
-  !    case default
-  !      print *, "Invalid name for glacier flow algorithm: " // trim(g%name_flow)
-  !      stop -1
-  !  end select
+      case ('none')
+        g%on = .false.
+        g%update => NULL() ! will fail if called, by design
 
-  !  ! Set exact solution procedure
-  !  select case (g%name_soln)
-  !    case ('none')
-  !      g%on_soln = .false.
-  !      g%solve => NULL() ! will seg-fault if called, by design
-  !    case ('bueler_isothermal_a')
-  !      g%on_soln = .true.
-  !      g%solve => solve_bueler_isothermal_a 
-  !    case default
-  !      print *, "Invalid name for exact solution: " // trim(g%name_soln)
-  !      stop -1
-  !  end select
+      case ('mahaffy_isothermal_nonsliding')
+        g%on_soln = .false.
+        g%solve => NULL() ! NOT YET IMPLEMENTED
+    
+      case default
+        print *, "Invalid name for ice model method: " // trim(p%ice_name)
+        stop 'Stopped.'
 
-  !end subroutine init
+    end select
+
+    ! Set exact solution procedure
+    select case (p%ice_soln_name)
+
+      case ('none')
+        g%on_soln = .false.
+        g%solve => NULL() ! will seg-fault if called, by design
+
+      case default
+        print *, "Invalid name for exact solution: " // trim(p%ice_soln_name)
+        stop 'Stopped.'
+
+    end select
+
+    ! Parse boundary condition names from comma-separated list
+    comma(1) = index(p%ice_bc_name, ',')
+    comma(2) = index(p%ice_bc_name((comma(1)+1):), ',')+comma(1)
+    comma(3) = index(p%ice_bc_name((comma(2)+1):), ',')+comma(2)
+    nbc_name = p%ice_bc_name(1:comma(1)-1)
+    ebc_name = p%ice_bc_name(comma(1)+1:comma(2)-1)
+    sbc_name = p%ice_bc_name(comma(2)+1:comma(3)-1)
+    wbc_name = p%ice_bc_name(comma(3)+1:)
+
+    ! Set boundary condition procedures (uses subroutine due to repitition)
+    call set_bc_pointer(nbc_name, g%apply_nbc)
+    call set_bc_pointer(ebc_name, g%apply_ebc)
+    call set_bc_pointer(sbc_name, g%apply_sbc)
+    call set_bc_pointer(wbc_name, g%apply_nbc)
+
+  end subroutine init
 
 
-  !! ---------------------------------------------------------------------------
-  !! SUB: Parse boundary condition names and associate procedure pointers
-  !! ---------------------------------------------------------------------------
-  !subroutine set_bc_pointer(str, ptr)
+  ! ---------------------------------------------------------------------------
+  ! SUB: Parse boundary condition names and associate procedure pointers
+  ! ---------------------------------------------------------------------------
+  subroutine set_bc_pointer(str, ptr)
 
-  !  character(len=*), intent(in) :: str
-  !  procedure(bc_tmpl), pointer, intent(out) :: ptr
+    character(len=*), intent(in) :: str
+    procedure(bc_tmpl), pointer, intent(out) :: ptr
 
-  !  select case (str)
-  !    case ("no_ice")
-  !      ptr => NULL() ! NOT YET IMPLEMENTED 
-  !    case default
-  !      print *, "Invalid name for boundary condition: " // trim(str)
-  !      stop -1
-  !  end select
+    select case (str)
 
-  !end subroutine set_bc_pointer
+      case ('none')
+        ptr => NULL() ! will fail if called, by design. 
+    
+      case default
+        print *, "Invalid name for boundary condition: " // trim(str)
+        stop 'Stopped.'
+   
+    end select
+
+  end subroutine set_bc_pointer
 
 
   !! ---------------------------------------------------------------------------
