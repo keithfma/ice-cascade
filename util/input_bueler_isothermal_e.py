5841877465a86c0f54279ed793ecc4c8cfb883ee
keithma <keithma@bu.edu>
2015-06-15 21:00:31 -0400

    Mostly completed input_bueler_isothermal_e.py
    Still has "none" or [] for quantities not yet defined in the main program.

--- util/input_bueler_isothermal_e.py
+++ util/input_bueler_isothermal_e.py
@@ -31,38 +31,103 @@ import numpy as np
 import netCDF4 as nc
 import ice_cascade_tools as ict
 
+from matplotlib import pyplot as plt
+
 # parameters for Bueler test E
 M0 = 0.3 # [m/a]
 L = 750000. # [m]
 mumax = 2.5e-11*31556926. # [m/Pa/a]
 r1 = 200000. # [m]
 r2 = 700000. # [m]
-theta1 = 10 # [degrees]
-theta2 = 40 # [degrees]
+th1 = 10.*np.pi/180. # [radians]
+th2 = 40.*np.pi/180. # [radians]
+g = 9.81 # acceleration of gravity, [m/s2]
+rhoi = 910. # ice density, [kg/m3]
+A = 1.0e-16 # ice deformation coeff, [Pa-3 a-1]
 
 # general parameters
+ti = 0. # [a]
+tf = 25000. # [a]
+dt = 100. # model time step
+tw = tf # output steps
+lxy = 1.1*L # domain dimensions (final radius + 10%)
+descr = ('Benchmark case with exact solution (Bueler et al 2005, test E).'
+  'Isothermal, steady-state ice cap with sliding in a ice-stream-like sector'
+  'only.')
 
 # main function
 def main(filename, nxy):
 
   # coordinate grids
+  (xy, dxy) = np.linspace(0.0, lxy, num = nxy, retstep = True, dtype = np.float64)
+  (xx, yy) = np.meshgrid(xy, xy)
+  rr = np.sqrt(xx**2+yy**2)
+  thth = np.arctan2(yy, xx)
 
   # sliding coefficient
+  mask = rr > r1
+  mask = np.logical_and(mask, rr < r2)
+  mask = np.logical_and(mask, thth > th1)
+  mask = np.logical_and(mask, thth < th2)
+  mumu = np.zeros((nxy, nxy), dtype = np.float64)
+  mumu[mask] = 1.*( (4.*(rr[mask]-r1)*(r2-rr[mask])/((r2-r1)**2)) * 
+                     (4.*(thth[mask]-th1)*(th2-thth[mask])/((th2-th1)**2)) )
 
   # exact solution
+  gamma = 2.0/5.0*A*(rhoi*g)**3.0
+  mask = np.where(rr <= L)
+  ice_h_soln = np.zeros((nxy, nxy), dtype = np.float64)
+  ice_h_soln[mask] = (4.0*M0/gamma)**(1.0/8.0)*(L**(4.0/3.0)-rr[mask]**(4.0/3.0))**(3.0/8.0)
 
   # create and open new input file
   file = ict.new_input(filename, nxy, nxy)
   
   # define parameters and variables 
+  file.descr = descr
+  file.nx__1 = nxy
+  file.ny__1 = nxy
+  file.lx__m = lxy
+  file.ly__m = lxy
+  file.dx__m = dxy
+  file.dy__m = dxy
+  file.rhoi__kg_m3 = rhoi
+  file.grav__m_s2 = g
+  file.time_start__a = ti
+  file.time_finish__a = tf
+  file.time_step__a = dt
+  file.time_write__a = tw 
+  file.climate_name = 'none'
+  file.climate_param__var = []
+  file.ice_name = 'none'
+  file.ice_param__var = []
+  file.ice_bc_name__nesw = 'no_ice,no_ice,no_flux,no_flux'
+  file.ice_soln_name = 'none'
+  file.ice_soln_param__var = []
+  file.write_ice_q_surf = 1
+  file.write_ice_h = 1
+  file.write_ice_h_soln = 1 
+  file.write_ice_a_slid = 1
+  file.variables['x'][:] = xy
+  file.variables['y'][:] = xy
+  file.variables['topo'][:,:] = 0.
+  file.variables['ice_h'][:,:] = ice_h_soln
+  file.variables['ice_h_soln'][:,:] = ice_h_soln
+  file.variables['ice_a_slid'][:,:] = mumu
 
+  # # DEBUG
+  # print mumu
+  # plt.imshow(mumu, 
+  #     origin = 'lower', 
+  #     extent = (xy[0], xy[-1], xy[0], xy[-1]))
+  # plt.colorbar()
+  # plt.show()
   
 # run as script
 if __name__ == '__main__':
 
   # defaults
   nxy = 51 
-  filename = 'bueler_isothermal_d_in_'+str(nxy)+'.nc' 
+  filename = 'bueler_isothermal_e_in_'+str(nxy)+'.nc' 
   
   # parse input arguments
   if len(sys.argv) == 2:
