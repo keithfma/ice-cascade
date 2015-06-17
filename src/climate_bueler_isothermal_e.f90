3bf336136d52dac062d8c2ae0185e2679dcc660e
keithma <keithma@bu.edu>
2015-06-17 16:08:43 -0400

    Cleaned up climate_bueler_isothermal_e
    Removed debugging code that has served it's purpose.

--- src/climate_bueler_isothermal_e.f90
+++ src/climate_bueler_isothermal_e.f90
@@ -3,7 +3,7 @@
 !
 ! Description: Test E is a steady state ice cap with sliding in 4
 !   ice-stream-like sectors and the same exact solution as in Test A. The
-!   procedures in this module compute the "compensitory accumulation" required
+!   procedures in this module compute the "compensatory accumulation" required
 !   to generate this solution. Enabled if climate_name is 'bueler_isothermal_e'
 !
 ! Parameters:
@@ -23,8 +23,6 @@
 !   doi:10.3189/172756505781829449
 ! 
 ! Public: init_bueler_isothermal_e, update_bueler_isothermal_e
-!
-! Private: 
 !
 ! =============================================================================
 
@@ -73,12 +71,12 @@ contains
 
     integer :: i, j
 
-    ! ! expect exactly XXX parameters
-    ! if (size(p%climate_param) .ne. XXX) then
-    !   print *, 'Invalid climate parameters: bueler_isothermal_d requires &
-    !            &exactly 6 parameters.'
-    !   stop
-    ! end if
+    ! expect exactly 8 parameters
+    if (size(p%climate_param) .ne. 8) then
+      print *, 'Invalid climate parameters: bueler_isothermal_e requires &
+               &exactly 8 parameters.'
+      stop
+    end if
 
     ! rename parameters
     M0 = p%climate_param(1)
@@ -172,14 +170,12 @@ contains
       do i = 1, p%nx
         rr(i,j) = sqrt(s%x(i)*s%x(i)+s%y(j)*s%y(j))
         thth(i,j) = atan2(s%y(j), s%x(i))
-        !! DEBUG
-        !print *, s%x(i), s%y(j), thth(i,j)
       end do
     end do
 
     ! pre-compute constants 
-    gam = 2.0_rp/5.0_rp*A*(p%rhoi*p%grav)**3.0_rp !#ok
-    cv = (4.0_rp*M0/gam)**(1.0_rp/8.0_rp) !#ok
+    gam = 2.0_rp/5.0_rp*A*(p%rhoi*p%grav)**3.0_rp 
+    cv = (4.0_rp*M0/gam)**(1.0_rp/8.0_rp) 
 
     ! pre-compute grids for compensatory accum (only needed in sliding areas)
     ww = 0.0_rp
@@ -189,52 +185,18 @@ contains
     mm_p = 0.0_rp
     compensatory = 0.0_rp
     where ((rr .gt. r1) .and. (rr .lt. r2) .and. (thth .gt. th1) .and. (thth .lt. th2))
-      ww = L**(4.0_rp/3.0_rp)-rr**(4.0_rp/3.0_rp) !#ok
-      hhv = cv*ww**(3.0_rp/8.0_rp) !#ok
-      hhv_p = -cv/2.0_rp*rr**(1.0_rp/3.0_rp)*ww**(-5.0_rp/8.0_rp) !#ok
+      ww = L**(4.0_rp/3.0_rp)-rr**(4.0_rp/3.0_rp) 
+      hhv = cv*ww**(3.0_rp/8.0_rp) 
+      hhv_p = -cv/2.0_rp*rr**(1.0_rp/3.0_rp)*ww**(-5.0_rp/8.0_rp) 
       hhv_pp = -cv/6.0_rp*ww**(-13.0_rp/8.0_rp) * &
-          (rr**(-2.0_rp/3.0_rp)*ww+5.0_rp/2.0_rp*rr**(2.0_rp/3.0_rp)) !#ok
+          (rr**(-2.0_rp/3.0_rp)*ww+5.0_rp/2.0_rp*rr**(2.0_rp/3.0_rp)) 
       mm = m_max*4.0_rp*(rr-r1)*(r2-rr)/(r2-r1)**2 * & 
-          4.0_rp*(thth-th1)*(th2-thth)/(th2-th1)**2 !#ok
-
+          4.0_rp*(thth-th1)*(th2-thth)/(th2-th1)**2 
       mm_p = m_max*4.0_rp*(r1+r2-2.0_rp*rr)/(r2-r1)**2 * & 
           4.0_rp*(thth-th1)*(th2-thth)/(th2-th1)**2 
-
       compensatory = -p%rhoi*p%grav*(hhv**2*hhv_p*(mm/rr+mm_p) + &
           mm*hhv*(2.0_rp*hhv_p**2+hhv*hhv_pp))
-
     end where
-
-
-    !! DEBUG
-    !! check input parameters
-    !print *, 'Ad: ', A, ' 1/Pa3/a'
-    !print *, 'GAMMA: ', gam, ' 1/m3/a'
-    !print *, 'GAMMA: ', gam/31556926.0_rp, ' 1/m3/s'
-    !print *, 'M0: ', M0, ' m/a'
-    !print *, 'L: ', L, ' m' 
-    !print *, 'm_max: ', m_max, ' m/a/Pa'
-    !print *, 'm_max: ', m_max/31556926.0_rp, ' m/s/Pa'
-    !print *, 'r1: ', r1, ' m' 
-    !print *, 'r2: ', r2, ' m'  
-    !print *, 'th1: ', th1, ' rad' 
-    !print *, 'th1: ', th1*180.0_rp/3.1415_rp, ' degrees' 
-    !print *, 'th2: ', th2, ' rad' 
-    !print *, 'th2: ', th2*180.0_rp/3.1415_rp, ' degrees' 
-    !! check computed values
-    !print *, 'compensatory max: ', maxval(compensatory+M0), ' m/a'
-    !print *, 'compensatory min: ', minval(compensatory+M0), ' m/a'
-    !!print *, mm 
-    !!do j = 1, p%ny
-    !!  do i = 1, p%nx
-    !!    print *, ''
-    !!    print *, 'r: ', rr(i,j)
-    !!    print *, 'th: ', thth(i,j)
-    !!    print *, 'w: ', ww(i,j)
-    !!    print *, 'Hvpp: ', hhv_pp(i,j)  
-    !!  end do
-    !!end do
-    !! END DEBUG
 
   end subroutine init_bueler_isothermal_e
 
@@ -251,7 +213,7 @@ contains
     where (rr .lt. L)
       s%ice_q_surf = M0+compensatory
     elsewhere
-      s%ice_q_surf = 0.0_rp !Mn
+      s%ice_q_surf = Mn
     end where
 
   end subroutine update_bueler_isothermal_e
